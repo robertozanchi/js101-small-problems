49eab649c33a32793a7b93f8b88c991e17bcd16c
Roberto Zanchi <r.zanchi@gmail.com>
2020-03-07 15:27:54 -0500

    continue easy 4 exercises

---
+++ easy-4/easy-4-5.js
@@ -0,0 +1,52 @@
+// Palindromic Strings (Part 2)
+
+/*
+Algorithm:
+- convert string to small-cap, alphanumeric characters only
+- check if string is palindrome with isPalindrome function
+
+*/
+
+function isPalindrome(string) {
+  return (
+    string ===
+    string
+      .split("")
+      .reverse()
+      .join("")
+  );
+}
+
+function cleanUp(text) {
+  let cleanText = "";
+
+  for (let idx = 0; idx < text.length; idx += 1) {
+    if (
+      (text[idx] >= "a" && text[idx] <= "z") ||
+      (text[idx] >= "A" && text[idx] <= "A") ||
+      (text[idx] >= "0" && text[idx] <= "9")
+    ) {
+      cleanText += text[idx];
+    }
+  }
+
+  return cleanText;
+}
+
+function isRealPalindrome(text) {
+  text = cleanUp(text.toLowerCase());
+  return isPalindrome(text);
+}
+
+// LS solution
+function isRealPalindrome(string) {
+  string = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return isPalindrome(string);
+}
+
+console.log(isRealPalindrome("madam")); // true
+console.log(isRealPalindrome("Madam")); // true (case does not matter)
+console.log(isRealPalindrome("Madam, I'm Adam")); // true (only alphanumerics matter)
+console.log(isRealPalindrome("356653")); // true
+console.log(isRealPalindrome("356a653")); // true
+console.log(isRealPalindrome("123ab321")); // false
