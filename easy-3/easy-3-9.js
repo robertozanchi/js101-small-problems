da0583b49c71fe1ef468bdc97cb721cdc41b3fbf
Roberto Zanchi <r.zanchi@gmail.com>
2020-03-06 20:17:46 -0500

    continue easy 3 exercises

--- easy-3/easy-3-9.js
+++ easy-3/easy-3-9.js
@@ -1,1 +1,47 @@
 // Clean up the words
+
+/*
+Problem
+input:
+- a string
+
+output:
+- a string with all of the non-alphabetic characters replaced by spaces
+
+rules:
+- for one or more non-alphabetic characters in a row, replace with one space
+- validation?
+
+Examples
+cleanUp("---what's my +*& line?");    // " what s my line "
+
+Data
+strings
+
+Algoritm
+- function with parameter string of type string
+- set finalString = ""
+- loop from 0 through string length -1
+  - if !a < string[i] < z
+    - if finalString[i - 1] === " "
+      - continue
+    finalString += " "
+return finalString
+*/
+
+function cleanUp(string) {
+  let cleanString = "";
+  for (let idx = 0; idx < string.length; idx += 1) {
+    if (!/^[A-Z]$/i.test(string.charAt(idx))) {
+      if (cleanString.charAt(cleanString.length - 1) === " ") {
+        continue;
+      }
+      cleanString += " ";
+    } else {
+      cleanString += string.charAt(idx);
+    }
+  }
+  console.log(cleanString);
+  return cleanString;
+}
+cleanUp("---what's my +*& line?"); // " what s my line "
