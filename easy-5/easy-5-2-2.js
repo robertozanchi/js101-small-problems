622fd1807c081e2ee13944ac0e57dc17b9fa497e
Roberto Zanchi <r.zanchi@gmail.com>
2020-05-15 23:48:24 -0400

    redo easy 5

--- easy-5/easy-5-2-2.js
+++ easy-5/easy-5-2-2.js
@@ -1,16 +1,7 @@
-// 1 Cute Angles
-
 function dms(angle) {
-  while (angle < 0) {
-    angle += 360;
-  }
-
-  angle = angle % 360;
-
   let degrees = parseInt(angle, 10);
   let minutes = parseInt((angle - degrees) * 60, 10);
   let seconds = parseInt(((angle - degrees) * 60 - minutes) * 60, 10);
-
   return console.log(`${degrees}˚${padZeroes(minutes)}'${padZeroes(seconds)}"`);
 }
 
@@ -18,15 +9,3 @@ function padZeroes(number) {
   var numString = String(number);
   return numString.length < 2 ? "0" + numString : numString;
 }
-
-dms(30); // 30°00'00"
-dms(76.73); // 76°43'48"
-dms(254.6); // 254°35'59"
-dms(93.034773); // 93°02'05"
-dms(0); // 0°00'00"
-dms(360); // 360°00'00" or 0°00'00"
-console.log();
-dms(-1); // 359°00'00"
-dms(400); // 40°00'00"
-dms(-40); // 320°00'00"
-dms(-420); // 300°00'00"
