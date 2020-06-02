11a38c6c9fb9510f108018f4a68e28a03bfb8dfc
Roberto Zanchi <r.zanchi@gmail.com>
2020-06-02 18:46:42 -0400

    redo medium 1

---
+++ medium-1/medium-1-6.js
@@ -0,0 +1,18 @@
+// Fibonacci Numbers (Recursion)
+// June 2, 2020
+
+let fibonacci = function (num) {
+  if (num === 1 || num === 2) {
+    return 1;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+};
+
+console.log(fibonacci(1)); // 1
+console.log(fibonacci(2)); // 1
+console.log(fibonacci(3)); // 2
+console.log(fibonacci(4)); // 3
+console.log(fibonacci(5)); // 5
+console.log(fibonacci(12)); // 144
+console.log(fibonacci(20)); // 6765
